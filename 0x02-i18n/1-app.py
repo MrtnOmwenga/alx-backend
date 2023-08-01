736427fe0919629781506b8b1a8d9ab80fb37bcc
MrtnOmwenga <martin36449@gmail.com>
2023-08-02 00:24:03 +0300

    Task 1: Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+i18n exercises
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Babel i18n configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route("/")
+def hello() -> str:
+    """ Base route for application """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
