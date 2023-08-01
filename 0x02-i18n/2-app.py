58ad7f99f07595b6073a08c79366162bffcb9216
MrtnOmwenga <martin36449@gmail.com>
2023-08-02 01:31:34 +0300

    Task 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -28,7 +28,7 @@ def get_locale() -> str:
 @app.route("/")
 def hello() -> str:
     """ Base route for application """
-    return render_template('3-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
