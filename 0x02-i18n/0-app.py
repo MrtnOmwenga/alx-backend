d00b3c87efcfbe731d100ed4b71f697cbeb4c131
MrtnOmwenga <martin36449@gmail.com>
2023-08-01 23:53:20 +0300

    Added documentation and pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+"""
+i18n exercises
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
+    """ Base route for application """
     return render_template('index.html')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run()
