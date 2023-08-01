a3eb4f2377760477c81ae20c01c83f86c28d87be
MrtnOmwenga <martin36449@gmail.com>
2023-08-02 00:38:45 +0300

    Pycodestyle

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,7 +15,9 @@ class Config:
 
 @babel.localeselector
 def get_locale():
+    """ Detemines the best match with supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
